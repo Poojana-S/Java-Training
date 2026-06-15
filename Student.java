ea64bd6ed9296af5930fcb6d50268fd819578382
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-15 14:33:54 +0530

    Student Info

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student {
+    public static void main(String[] args) {
+        String name = "Poojana";
+        int rollNo = 101;
+        String department = "Computer Science Engineering";
+        int age = 20;
+
+        System.out.println("Student Details");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Department: " + department);
+        System.out.println("Age: " + age);
+    }
+}
