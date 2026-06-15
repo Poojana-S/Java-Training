42a45599ee6709256e5e169815dba1697678c914
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-15 14:43:46 +0530

    Arithmetic operations

---
+++ Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    public static void main(String args[]){
+        int x=5;
+        System.out.println(x+x-x*x);
+        // 5+5-5*5
+        // 5+5-25
+        // 10-25
+        // -15
+    }
+}
