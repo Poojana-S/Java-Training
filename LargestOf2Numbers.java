14387d1319b0ea6bc078790d710ac909d778f5be
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-15 14:39:01 +0530

    Largest Of 2 Numbers

---
+++ LargestOf2Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LargestOf2Numbers {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        if(num1>num2){
+            System.out.println(num1 + " is the largest.");
+        } else {
+            System.out.println(num2 + " is the largest.");
+        }
+        sc.close();
+    }
+}
