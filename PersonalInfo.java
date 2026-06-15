3cd956308717363ca8fd86d7d631219dcbe2f831
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-15 14:33:33 +0530

    Personal Info

---
+++ PersonalInfo.java
@@ -0,0 +1,14 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Poojana S";
+        int age = 20;
+        String address = "Perundurai, Erode, Tamil Nadu, India";
+        String phoneNumber = "7904060460";
+
+        System.out.println("Personal Information");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
