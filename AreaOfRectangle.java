a0e04e77a39be2a326f81156c39de6a53fee8a68
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-15 14:35:04 +0530

    Area of rectangle

---
+++ AreaOfRectangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = sc.nextDouble();
+
+        double area = length * width; // Calculate area
+
+        System.out.println("The area of the rectangle is: " + area);
+        sc.close();
+    }
+}
