3dd30b2edadbc573d172bbc37d8e2bd14999dd7e
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-17 14:29:31 +0530

    String reverse

--- Day 3/StringReverse.java
+++ Day 3/StringReverse.java
@@ -5,7 +5,7 @@ public class StringReverse{
         Scanner sc = new Scanner(System.in);
         String s1=sc.next();
         for(int i=s1.length()-1;i>=0;i--){
-            System.out.println(s1.charAt(i));
+            System.out.print(s1.charAt(i));
         }
         sc.close();
     }
